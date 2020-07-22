51008c66117bc82ecc2c4e64eee1857b82123433
hannahjorich <hannah.jorich@gmail.com>
2020-07-22 14:19:13 -0600

    pseudo code

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+// Display current time and day 
+
+// create timeblocks for standard business hours  in index 
+
+// Create a function that save task to local storage 
+
+// Create color coded function to indicate whether it is in the pasat, present or future 
+
+// Should the task be saved once the page can refresh???
